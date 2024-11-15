31e4dadcd88257a4fd77e42963d49ce4a86e7a82
Ivan <iv.kovatchev@gmail.com>
2024-11-15 17:29:41 +0200

    add small changes in the expense_item

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
@@ -8,6 +12,13 @@ enum Category {
   leisure,
   work
 }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
 
 class Expense {
   final String id;
@@ -22,4 +33,8 @@ class Expense {
     this.date,
     this.category
   ) : id = uuid.v4();
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
